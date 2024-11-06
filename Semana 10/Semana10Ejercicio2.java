42cb4d7efd5e5695f6661209d8ff7ef729936473
damarquezn <141339195+damarquezn@users.noreply.github.com>
2024-11-06 11:45:31 -0500

    Add files via upload

---
+++ Semana 10/Semana10Ejercicio2.java
@@ -0,0 +1,16 @@
+package Semana10;
+
+import java.util.ArrayList;
+
+public class Semana10Ejercicio2 {
+    public static void main(String[] args) {
+        ArrayList<String> nombres = new ArrayList();
+        nombres.add("Leandro");
+        nombres.add("Juan");
+        nombres.add("Daniel");
+
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
